d430fc79f0d1039f5e7e6e78968f4dbe9f9abbbc
Sesank2005 <sesankreddygontu@gmail.com>
2025-01-06 09:37:01 +0530

    Update mywork.js

--- src/assets/mywork.js
+++ src/assets/mywork.js
@@ -1,6 +1,7 @@
 import  work1 from '../assets/work-1.png'
 import work2 from '../assets/work-2.png'
 import work3 from '../assets/work-3.png'
+import work4 from '../assets/work-4.png'
 const mywork_data=[
     {
         w_no:"1",
@@ -23,19 +24,19 @@ const mywork_data=[
 {
     w_no:"4",
     w_name:"web-design",
-    w_img:work1
+    w_img:work4
 
 },
 {
 w_no:"5",
 w_name:"web-design",
-w_img:work2
+w_img:work3
 
 },
 {
 w_no:"6",
 w_name:"web-design",
-w_img:work3
+w_img:work1
 
 },
 ]
